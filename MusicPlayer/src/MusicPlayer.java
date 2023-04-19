36c7f6309eceaa2c03f5232b998933193e5d383d
75251501 <75251501@L829WD210022.edu.cbe.ab.ca>
2023-04-19 15:02:09 -0600

    initial commit

---
+++ MusicPlayer/src/MusicPlayer.java
@@ -0,0 +1,8 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MusicPlayer extends JFrame{
+	private JPanel contentPane;
+	private JButton fourSeasons;
+}
