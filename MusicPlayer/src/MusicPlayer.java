ca56cef9115e21526fe2f449d72b6aaaee7cf480
75251501 <75251501@L829WD210022.edu.cbe.ab.ca>
2023-04-20 13:40:11 -0600

    commit

--- MusicPlayer/src/MusicPlayer.java
+++ MusicPlayer/src/MusicPlayer.java
@@ -1,8 +1,32 @@
+import java.awt.EventQueue;
+
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+
 public class MusicPlayer extends JFrame{
 	private JPanel contentPane;
 	private JButton fourSeasons;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MusicPlayer frame = new MusicPlayer();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public MusicPlayer() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 600);
+		
+	}
 }
