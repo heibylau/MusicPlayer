31d93bf213ac5bc52f6e89744865babfe6069844
heibylau <heibyl2@educbe.ca>
2023-04-25 18:59:11 -0600

    remember to redownload the music as .wav

--- MusicPlayer/src/FourSeasons.java
+++ MusicPlayer/src/FourSeasons.java
@@ -20,18 +20,18 @@ public class FourSeasons extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	//files
-	private static final String SPRING1 = "music/Spring_1st_movement.mp3";
-	private static final String SPRING2 = "music/Spring_2nd_movement.mp3";
-	private static final String SPRING3 = "music/Spring_3rd_movement.mp3";
-	private static final String SUMMER1 = "music/Summer_1st_movement.mp3";
-	private static final String SUMMER2 = "music/Summer_2nd_movement.mp3";
-	private static final String SUMMER3 = "music/Summer_3rd_movement.mp3";
-	private static final String AUTUMN1 = "music/Autumn_1st_movement.mp3";
-	private static final String AUTUMN2 = "music/Autumn_2nd_movement.mp3";
-	private static final String AUTUMN3 = "music/Autumn_3rd_movement.mp3";
-	private static final String WINTER1 = "music/Winter_1st_movement.mp3";
-	private static final String WINTER2 = "music/Winter_2nd_movement.mp3";
-	private static final String WINTER3 = "music/Winter_3rd_movement.mp3";
+	private static final String SPRING1 = "music/Spring_1st_movement.wav";
+	private static final String SPRING2 = "music/Spring_2nd_movement.wav";
+	private static final String SPRING3 = "music/Spring_3rd_movement.wav";
+	private static final String SUMMER1 = "music/Summer_1st_movement.wav";
+	private static final String SUMMER2 = "music/Summer_2nd_movement.wav";
+	private static final String SUMMER3 = "music/Summer_3rd_movement.wav";
+	private static final String AUTUMN1 = "music/Autumn_1st_movement.wav";
+	private static final String AUTUMN2 = "music/Autumn_2nd_movement.wav";
+	private static final String AUTUMN3 = "music/Autumn_3rd_movement.wav";
+	private static final String WINTER1 = "music/Winter_1st_movement.wav";
+	private static final String WINTER2 = "music/Winter_2nd_movement.wav";
+	private static final String WINTER3 = "music/Winter_3rd_movement.wav";
 
 	//button
 	private JButton btnPlay;
